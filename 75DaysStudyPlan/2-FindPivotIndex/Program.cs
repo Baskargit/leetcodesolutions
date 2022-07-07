79de217714ce23c59cbcb228b31f3020403174d0
Baskaran <basskar142@gmail.com>
2022-07-07 21:00:40 +0530

    Day 3 solved problems

---
+++ 75DaysStudyPlan/2-FindPivotIndex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_FindPivotIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PivotIndex(new int[] {1,7,3,6,5,6}));
+            Console.WriteLine(PivotIndex(new int[] {1,2,3}));
+        }
+
+        public static int PivotIndex(int[] nums)
+        {
+            int wholeSum = 0, runningSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                wholeSum += nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                wholeSum -= nums[i];
+                
+                if (wholeSum == runningSum)
+                {
+                    return i;
+                }
+
+                runningSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
+
+// Link: https://leetcode.com/problems/find-pivot-index/
---
+++ 75DaysStudyPlan/3-IsomorphicStrings/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsIsomorphic("egg", "add"));
+            Console.WriteLine(IsIsomorphic("foo", "bar"));
+            Console.WriteLine(IsIsomorphic("paper", "title"));
+
+            // First wrong submission test case
+            Console.WriteLine(IsIsomorphic("badc", "baba"));
+        }
+
+        public static bool IsIsomorphic(string s, string t)
+        {
+            if (s.Equals(t))
+            {
+                return true;
+            }
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var dic = new Dictionary<char, char>();
+            var usedChars = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    if (t[i] != dic[s[i]])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (!usedChars.ContainsKey(t[i]))
+                    {
+                        dic.Add(s[i], t[i]);
+                        usedChars.Add(t[i], t[i]);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
+// https://leetcode.com/problems/isomorphic-strings/

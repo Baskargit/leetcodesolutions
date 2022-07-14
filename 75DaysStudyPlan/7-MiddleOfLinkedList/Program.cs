23a37ad5670dfe013c54677ad4170cd0239c2d7f
Baskaran <basskar142@gmail.com>
2022-07-14 08:47:39 +0530

    Partially completed LL problems

---
+++ 75DaysStudyPlan/7-MiddleOfLinkedList/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode? middleNode = null;
+
+            middleNode = MiddleNode(new ListNode(1));
+            Console.WriteLine(middleNode?.val);
+
+            middleNode = MiddleNode(new ListNode(1,new ListNode(2)));
+            Console.WriteLine(middleNode?.val);
+
+            middleNode = MiddleNode(new ListNode(1,new ListNode(2,new ListNode(3))));
+            Console.WriteLine(middleNode?.val);
+
+            middleNode = MiddleNode(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4)))));
+            Console.WriteLine(middleNode?.val);
+
+            middleNode = MiddleNode(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5))))));
+            Console.WriteLine(middleNode?.val);
+            
+            middleNode = MiddleNode(new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5, new ListNode(6)))))));
+            Console.WriteLine(middleNode?.val);
+        }
+
+        public static ListNode? MiddleNode(ListNode? head)
+        {
+            ListNode? nextNextNode = head;
+
+            while (head != null)
+            {
+                if (nextNextNode == null || nextNextNode?.next == null)
+                {
+                    return head;
+                }
+
+                nextNextNode = nextNextNode?.next?.next;
+                head = head?.next;
+            }
+
+            return head;
+        }
+    }
+
+    public class ListNode {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val=0, ListNode? next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+// https://leetcode.com/problems/middle-of-the-linked-list
---
+++ 75DaysStudyPlan/8-LinkedListnkedListCycleII/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var headNode = new ListNode(3);
+            headNode.next = new ListNode(2);
+            headNode.next.next = new ListNode(0);
+            headNode.next.next.next = new ListNode(-4);
+            headNode.next.next.next.next = headNode.next;
+
+            // var temp = headNode;
+            // while (temp != null)
+            // {
+            //     Console.WriteLine(temp.val);
+            //     temp = temp.next;
+            // }
+
+            ListNode? cycleNode = DetectCycle(headNode);
+            if (cycleNode != null)
+            {
+                Console.WriteLine(cycleNode.val);
+            }
+        }
+
+        // Naive approach
+        // Memory: O(n)
+        public static ListNode? DetectCycle(ListNode? head) 
+        {
+            var nodes = new List<ListNode>();
+
+            while (head != null)
+            {
+                var node = nodes.FirstOrDefault(x => x.Equals(head));
+                if (node != null)
+                {
+                    return node;
+                }
+                nodes.Add(head);
+                head = head?.next;
+            }
+
+            return null;
+        }
+    }
+
+    public class ListNode {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val=0, ListNode? next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+// https://leetcode.com/problems/linked-list-cycle-ii/
